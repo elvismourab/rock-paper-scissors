a22b1359d8c16e90dc6706f614f2963bc7971994
elvismourab <216804366+elvismourab@users.noreply.github.com>
2025-12-16 06:55:02 -0300

    feat: remove the logic that plays 5 rounds

--- script.js
+++ script.js
@@ -60,21 +60,11 @@ function playGame() {
         return console.log("It's a draw!");
     }
 
-    while (humanScore < 5 || computerScore < 5) {
-        console.log('humanScore: ', humanScore);
-        console.log('computerScore: ', computerScore);
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        if (humanScore === 5) {
-            alert('Congratulations! You win!');
-            break;
-        }
-        if (computerScore === 5) {
-            alert('Game Over! Computer wins!');
-            break;
-        }
-    };
+    console.log('humanScore: ', humanScore);
+    console.log('computerScore: ', computerScore);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
 }
 
 playGame();
